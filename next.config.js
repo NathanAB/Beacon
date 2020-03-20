977d048a8078c70f9d21b1cb109fd6add54bb1ba
Nathan Bakirci <nathanb92@gmail.com>
2020-03-20 19:39:40 -0400

    Add api url env var to next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = withImages(
     cssModules: true,
     env: {
       REACT_APP_MAPS_API_KEY: process.env.REACT_APP_MAPS_API_KEY,
+      REACT_APP_API_URL: process.env.REACT_APP_API_URL,
     },
   }),
 );
