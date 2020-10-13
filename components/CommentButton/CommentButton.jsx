25044f8af1862e7da918e5f51582943a1444f876
Nathan Bakirci <nathanb92@gmail.com>
2020-10-13 12:28:37 -0400

    Update comment teaser to be a popover

--- components/CommentButton/CommentButton.jsx
+++ components/CommentButton/CommentButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactGA from 'react-ga';
 import { Icon } from '@material-ui/core';
+import Popover from 'react-popover';
 import styles from './CommentButton.module.css';
 
 import Modal from '../Modal/Modal';
+import Paper from '../Paper/Paper';
 
 export default function CommentButton() {
   const [isModalOpen, setModalOpen] = React.useState(false);
@@ -15,30 +17,46 @@ export default function CommentButton() {
     setModalOpen(true);
   };
 
+  const popoverProps = {
+    isOpen: isModalOpen,
+    preferPlace: 'below',
+    // place: this.state.place,
+    onOuterAction: () => setModalOpen(false),
+    body: [
+      <Paper withShadow>
+        <div className={styles.popoutInner}>
+          <h6>Have something to contribute?</h6>
+          <br />
+          <p>
+            Comments are coming soon! In the meantime, we would love to hear your tips and
+            suggestions at{' '}
+            <ReactGA.OutboundLink
+              className={styles.contactLink}
+              to="mailto:contact@beacondates.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              eventLabel="Comment modal contact link"
+            >
+              contact@beacondates.com
+            </ReactGA.OutboundLink>
+          </p>
+        </div>
+      </Paper>,
+    ],
+  };
+
   return (
     <>
-      <button type="button" onClick={onClick} className={styles.container}>
-        <Icon color="primary">comment</Icon>
-      </button>
-      <Modal
+      <Popover {...popoverProps}>
+        <button type="button" onClick={onClick} className={styles.container}>
+          <Icon color="primary">comment</Icon>
+        </button>
+      </Popover>
+      {/* <Modal
         isOpen={isModalOpen}
         closeModal={() => setModalOpen(false)}
         title="Have something to contribute?"
-      >
-        <p>
-          Comments are coming soon! In the meantime, we would love to gather your tips and
-          suggestions at{' '}
-          <ReactGA.OutboundLink
-            className={styles.contactLink}
-            to="mailto:contact@beacondates.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            eventLabel="Comment modal contact link"
-          >
-            contact@beacondates.com
-          </ReactGA.OutboundLink>
-        </p>
-      </Modal>
+      ></Modal> */}
     </>
   );
 }
