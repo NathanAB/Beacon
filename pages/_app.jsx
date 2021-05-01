45ba92fadaedc0e2580598f25b2db2817190b132
Nathan Bakirci <nathanb92@gmail.com>
2021-05-01 19:53:55 -0400

    Remove tailwind import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -18,7 +18,6 @@ import Constants from '../constants';
 import 'react-markdown-editor-lite/lib/index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-tabs/style/react-tabs.css';
-import 'tailwindcss/tailwind.css';
 import '../css/index.css';
 
 export default class BeaconApp extends App {
